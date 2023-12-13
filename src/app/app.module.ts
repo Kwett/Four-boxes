c779843ac3c8157e85af35e76046918000786207
Clément COUDRIN <coudrin.clement@gmail.com>
2023-12-13 15:45:27 +0100

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BlockComponent } from "./block/block.component";
+
+@NgModule({
+  declarations: [AppComponent, BlockComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
